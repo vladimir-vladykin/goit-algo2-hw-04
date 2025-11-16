a315feecc0ea246373fb30d5319b86ea9262bbec
Volodymyr Vladykin <vova@pocketguard.com>
2025-11-16 13:42:04 +0200

    Basic impl of extended methods for Trie

--- trie_task.py
+++ trie_task.py
@@ -3,10 +3,27 @@ from trie import Trie
 
 class Homework(Trie):
     def count_words_with_suffix(self, pattern) -> int:
-        pass
+        if not isinstance(pattern, str):
+            raise TypeError(
+                f"Illegal argument for count_words_with_suffix: pattern = {pattern} must be a string"
+            )
+
+        count = 0
+        pattern = pattern.lower()
+        all_keys = self.keys()
+        for key in all_keys:
+            if key.lower().endswith(pattern):
+                count += 1
+
+        return count
 
     def has_prefix(self, prefix) -> bool:
-        pass
+        if not isinstance(prefix, str):
+            raise TypeError(
+                f"Illegal argument for has_prefix: prefix = {prefix} must be a string"
+            )
+
+        return len(self.keys_with_prefix(prefix)) > 0
 
 
 if __name__ == "__main__":
@@ -21,8 +38,12 @@ if __name__ == "__main__":
     assert trie.count_words_with_suffix("a") == 1  # banana
     assert trie.count_words_with_suffix("at") == 1  # cat
 
+    print("All 'count_words_with_suffix' tests passed")
+
     # Перевірка наявності префікса
     assert trie.has_prefix("app") == True  # apple, application
     assert trie.has_prefix("bat") == False
     assert trie.has_prefix("ban") == True  # banana
     assert trie.has_prefix("ca") == True  # cat
+
+    print("All 'has_prefix' tests passed")
